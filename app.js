03197efe6c97a09c58953a2a9c326ebf4652a012
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-11-08 11:05:37 -0600

    DOM Selectors
    query Selector

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
-// document.getElementById()
+// // document.getElementById()
 
-console.log(document.getElementById('task-title'));
+// console.log(document.getElementById('task-title'));
 
-// Get things from the element
+// // Get things from the element
 
-console.log(document.getElementById('task-title').id);
-console.log(document.getElementById('task-title').className);
+// console.log(document.getElementById('task-title').id);
+// console.log(document.getElementById('task-title').className);
 
-const taskTitle = document.getElementById('task-title');
+// const taskTitle = document.getElementById('task-title');
 
-// Change Styling
+// // Change Styling
 
-taskTitle.style.background = '#333';
-taskTitle.style.color = '#fff';
-taskTitle.style.padding = '5px';
-// document.getElementById('task-title').style.display = 'none';
+// taskTitle.style.background = '#333';
+// taskTitle.style.color = '#fff';
+// taskTitle.style.padding = '5px';
+// // document.getElementById('task-title').style.display = 'none';
 
-// Change content
-taskTitle.textContent = 'Task List';
-taskTitle.innerText = 'My Tasks';
-taskTitle.innerHTML = '<span style="color:red">Task List</span>';
+// // Change content
+// taskTitle.textContent = 'Task List';
+// taskTitle.innerText = 'My Tasks';
+// taskTitle.innerHTML = '<span style="color:red">Task List</span>';
+
+console.log(document.querySelector('#task-title'));
+console.log(document.querySelector('.card-title'));
+console.log(document.querySelector('h5'));
+
+document.querySelector('li').style.color = 'red';
+document.querySelector('ul li').style.color = 'blue';
+
+document.querySelector('li:last-child').style.color = 'orange';
+
+document.querySelector('li:nth-child(3)').style.color = 'brown';
+document.querySelector('li:nth-child(4)').textContent = 'Hello World';
+document.querySelector('li:nth-child(odd)').style.background = '#000';
+document.querySelector('li:nth-child(even)').style.background = '#000';
