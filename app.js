8bf67a7f5ee98adcb592b944305865a67190e435
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-11-09 11:49:13 -0600

    Traversing the DOM

--- app.js
+++ app.js
@@ -1,53 +1,8 @@
-// document.getElementsByClassName
+let val;
 
-// const items = document.getElementsByClassName('collection-item');
-// console.log(items);
-// console.log(items[0]);
-// items[0].style.color ='red';
-// items[3].textContent = 'Hello';
+const list = document.querySelector('ul.collection');
+const listItem = document.querySelector('li.collection-item:first-child');
 
-// const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
+val = listItem;
 
-// console.log(listItems);
-
-// document.getElementsByTagName
-
-// let lis = document.getElementsByTagName('li');
-// console.log(lis);
-// console.log(lis[0]);
-// lis[0].style.color ='red';
-// lis[3].textContent = 'Hello';
-
-// // Convert HTML Collection into array
-
-// lis = Array.from(lis);
-
-// lis.reverse();
-// lis.forEach(function(li, index){
-//   console.log(li.className);
-//   li.textContent = `${index}: Hello`;
-// });
-
-// console.log(lis);
-
-//document.querySelectorAll
-
-const items = document.querySelectorAll('ul.collection li.collection-item');
-
-items.forEach(function(item, index){
-item.textContent = `${index}: Hello`;
-});
-
-const liOdd = document.querySelectorAll('li:nth-child(odd)');
-const liEven = document.querySelectorAll('li:nth-child(even)');
-
-liOdd.forEach(function(li, index){
-  li.style.background = 'red';
-  });
-
-  for(let i = 0; i < liEven.length; i++){
-    liEven[i].style.background = "f4f4f4";
-  }
-
-
-console.log(items);
+console.log(val);
