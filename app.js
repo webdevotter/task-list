8d34f48cac748a02d5a4890feb5c0b796851c269
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-11-10 09:17:07 -0600

    Removing & Replacing Elements

--- app.js
+++ app.js
@@ -27,5 +27,25 @@ const list = document.querySelector('ul');
 // lis[0].remove();
 list.removeChild(lis[2]);
 
+// CLASSES & ATTR
 
-console.log(newHeading);
+const firstLi = document.querySelector('li:first-child');
+const link = firstLi.children[0];
+
+let val;
+
+// Classes
+link.classList.add('test');
+link.classList.remove('test');
+val = link;
+
+// Attributes
+val = link.getAttribute('href');
+val = link.setAttribute('href', 'http://google.com');
+link.setAttribute('title', 'Google');
+val = link.hasAttribute('title');
+val = link.removeAttribute('title');
+val = link;
+
+
+console.log(val);
