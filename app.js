ae556d66aab398fe61c80b6a6d4911ee03ed13b1
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-11-11 17:56:25 -0600

    Filter Tasks

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
-const filter = document.querySelector('filter');
+const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
 // Load all event listeners
@@ -14,6 +14,10 @@ function loadEventListeners() {
   form.addEventListener('submit', addTask);
   // Remove task event
   taskList.addEventListener('click', removeTask);
+  // Clear task event
+  clearBtn.addEventListener('click', clearTasks);
+  // Filter tasks event
+  filter.addEventListener('keyup', filterTasks);
 }
 
 // Add Task
@@ -53,4 +57,33 @@ function removeTask(e) {
     e.target.parentElement.parentElement.remove();
     }
   }
+}
+
+// Clear Tasks
+
+function clearTasks() {
+  // taskList.innerHTML = '';
+
+  // Faster
+  while(taskList.firstChild){
+    taskList.removeChild(taskList.firstChild);
+  }
+
+   // https://jsperf.com/innerhtml-vs-removechild
+
+}
+
+// Filter tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1){
+      task.style.display = 'block';
+
+    } else {
+      task.style.display = 'none';
+    }
+  });
 }
