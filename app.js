7ebdb71511b1ea8f7fb36778ce14a0d5ed7daa30
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-11-11 17:02:35 -0600

    content

--- app.js
+++ app.js
@@ -1,45 +1,7 @@
-//set local storage item
-// localStorage.setItem('firstName', 'John');
-// localStorage.setItem('age', '30');
+// Define UI Variables
 
-// //set session storage item
-
-// sessionStorage.setItem('name', 'Beth');
-
-//remove from storage
-// localStorage.removeItem('name');
-
-//get from storage
-
-// const name = localStorage.getItem('firstName');
-// const age = localStorage.getItem('age');
-// console.log(name, age);
-
-// localStorage.clear();
-
-document.querySelector('form').addEventListener('submit',function(e){
-    const task = document.getElementById('task').value;
-
-    let tasks;
-
-    if(localStorage.getItem('tasks') === null){
-      tasks =[];
-    } else{
-      tasks = JSON.parse(localStorage.getItem('tasks'));
-    }
-
-    task.push(task);
-
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-    alert('Task saved');
-
-    e.preventDEfault();
-  
-});
-
-
- const tasks = JSON.parse(localStorage.getItem('tasks'));
-
-tasks.forEach(function(task){
-  console.log(task);
-});
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('filter');
+const taskInput = document.querySelector('#task');
